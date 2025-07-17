33b6dedf8b0b0bb671a7b398b4c4694402f19be6
novaprospectCAU <novaprospect110a@gmail.com>
2025-07-17 16:09:19 +0900

    wip : main.cpp에서 window를 추가했습니다. esc키로 종료 가능합니다.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,40 @@
 
 int main()
 {
-  int arr[3];
-  std::cout << arr[4] << '\n';
+  if (SDL_Init(SDL_INIT_VIDEO))
+  {
+    SDL_Log("Failed to initialize SDL : %s", SDL_GetError());
+    return -1;
+  }
+
+  auto svectoraWindow = SDL_CreateWindow("Svectora", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 500, 0);
+  if (!svectoraWindow)
+  {
+    SDL_Log("Failed to create window! : %s", SDL_GetError());
+    return -1;
+  }
+
+  SDL_Event event;
+  bool isRunning{true};
+  while (isRunning)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+      case SDL_QUIT:
+        isRunning = false;
+        break;
+      }
+    }
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_ESCAPE])
+    {
+      isRunning = false;
+    }
+  }
+
+  SDL_DestroyWindow(svectoraWindow);
+  SDL_Quit();
   return 0;
 }
